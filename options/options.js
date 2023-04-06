d71430f0b61948a4dc7e098065082d22499b2e93
Ravinder sales <ravinder.y@crowdbotics.com>
2023-04-07 00:22:20 +0530

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- options/options.js
+++ options/options.js
@@ -1,12 +1,11 @@
-import { StyleSheet } from "react-native"
-
 export const globalOptions = {
-  name: "abcd-dev-74952",
-  url: "https://abcd-dev-74952.botics.co",
-  api: "https://abcd-dev-74952.botics.co/api/v1"
-}
+  "name": "abcd-dev-74952",
+  "url": "https://abcd-dev-74952.botics.co",
+  "api": "https://abcd-dev-74952.botics.co/api/v1",
+  "initialRoute": "Untitled1"
+};
 export const modulesOptions = {
   "@modules/app-menu": {
     copy: "Routes available!"
   }
-}
+};
